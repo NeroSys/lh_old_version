a1783428a9756f9872485546a4cf6258b870ad0a
Ткаченко Юрий <y.tkachenko@mvo.ru>
2018-04-27 15:58:30 +0300

    hotfix

--- src/Overrides/Catalog/Model/Catalog/ProductModel.php
+++ src/Overrides/Catalog/Model/Catalog/ProductModel.php
@@ -8,13 +8,57 @@ class ProductModel extends \ModelCatalogProduct
 {
     const PRODUCT_OPTION_IMAGE_PATH = null;
     const PRODUCT_OPTION_REQUIRED   = 1;
+    const PRICE_DISCOUNT_TYPE_PERCENT = 'persent';
+	const PRICE_DISCOUNT_TYPE_AMOUNT = 'amount';
 
     public function getAviableProductSpecifications(int $product_id):?array{
         $options = $this->getProductSpecificationOptionsFromDatabase($product_id);
         return $this->groupingOptionsAsSpecificationID($options);
     }
 
+	public function getProductOptionsOld($product_id) {
+		$product_option_data = array();
 
+		$product_option_query = $this->db->query("
+SELECT * FROM " . DB_PREFIX . "product_option po 
+LEFT JOIN `" . DB_PREFIX . "option` o ON (po.option_id = o.option_id) 
+LEFT JOIN " . DB_PREFIX . "option_description od ON (o.option_id = od.option_id) 
+WHERE po.product_id = '" . (int)$product_id . "' AND od.language_id = '" . (int)$this->config->get('config_language_id') . "' 
+ORDER BY o.sort_order");
+
+		foreach ($product_option_query->rows as $product_option) {
+			$product_option_value_data = array();
+
+			$product_option_value_query = $this->db->query("SELECT * FROM " . DB_PREFIX . "product_option_value pov LEFT JOIN " . DB_PREFIX . "option_value ov ON (pov.option_value_id = ov.option_value_id) LEFT JOIN " . DB_PREFIX . "option_value_description ovd ON (ov.option_value_id = ovd.option_value_id) WHERE pov.product_id = '" . (int)$product_id . "' AND pov.product_option_id = '" . (int)$product_option['product_option_id'] . "' AND ovd.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY ov.sort_order");
+
+			foreach ($product_option_value_query->rows as $product_option_value) {
+				$product_option_value_data[] = array(
+					'product_option_value_id' => $product_option_value['product_option_value_id'],
+					'option_value_id'         => $product_option_value['option_value_id'],
+					'name'                    => $product_option_value['name'],
+					'image'                   => $product_option_value['image'],
+					'quantity'                => $product_option_value['quantity'],
+					'subtract'                => $product_option_value['subtract'],
+					'price'                   => $product_option_value['price'],
+					'price_prefix'            => $product_option_value['price_prefix'],
+					'weight'                  => $product_option_value['weight'],
+					'weight_prefix'           => $product_option_value['weight_prefix']
+				);
+			}
+
+			$product_option_data[] = array(
+				'product_option_id'    => $product_option['product_option_id'],
+				'product_option_value' => $product_option_value_data,
+				'option_id'            => $product_option['option_id'],
+				'name'                 => $product_option['name'],
+				'type'                 => $product_option['type'],
+				'value'                => $product_option['value'],
+				'required'             => $product_option['required']
+			);
+		}
+
+		return $product_option_data;
+	}
 
     public function getProductOptions($product_id) {
         $product_option_data = array();
@@ -50,11 +94,23 @@ class ProductModel extends \ModelCatalogProduct
     private function groupingOptionsAsSpecificationID(array $data):?array{
         $options = array();
         foreach ($data as $option){
+        	if($option['price_discount'] > 0 && $option['price_discount_type'] == self::PRICE_DISCOUNT_TYPE_PERCENT) {
+				$price_old = $option['price'];
+				$price = $option['price'] - ($option['price'] * ($option['price_discount']/100));
+			} elseif ($option['price_discount'] > 0 && $option['price_discount_type'] == self::PRICE_DISCOUNT_TYPE_AMOUNT){
+				$price_old = $option['price'];
+				$price = $option['price'] - $option['price_discount'];
+			} else {
+				$price_old = 0;
+				$price = $option['price'];
+			}
+
 			$option['status'] = true;
 			$options[$option['id']]['id'] = $option['id'];
 			$options[$option['id']]['number'] = $option['id_erp'];
 			$options[$option['id']]['status'] = true;
-			$options[$option['id']]['price'] = $option['price'];
+			$options[$option['id']]['price'] = (float) $price;
+			$options[$option['id']]['price_old'] = (float) $price_old;
 			$option = $this->formatOption($option);
             $options[$option['id']]['options'][$option['option_id']] = $option;
         }
